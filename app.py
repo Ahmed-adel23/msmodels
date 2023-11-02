a507f763a9104fcdd4cf53c53406763085285e1a
Ahmed-adel23 <121492522+Ahmed-adel23@users.noreply.github.com>
2023-11-02 17:20:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from segmentation import MSPrediction4
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-CORS(flask_app, resources={r"/MSPrediction": {"origins": "*"}})
-CORS(flask_app, resources={r"/MSPrediction2": {"origins": "*"}})
-CORS(flask_app, resources={r"/MSPrediction3": {"origins": "*"}})
-CORS(flask_app, resources={r"/MSPrediction4": {"origins": "*"}})
+CORS(app, resources={r"/MSPrediction": {"origins": "*"}})
+CORS(app, resources={r"/MSPrediction2": {"origins": "*"}})
+CORS(app, resources={r"/MSPrediction3": {"origins": "*"}})
+CORS(app, resources={r"/MSPrediction4": {"origins": "*"}})
 
-api = Api(flask_app)
+api = Api(app)
 
 api.add_resource(MSPrediction , '/MSPrediction')
 api.add_resource(MSPrediction2, '/MSPrediction2')
@@ -20,4 +20,4 @@ api.add_resource(MSPrediction3, '/MSPrediction3')
 api.add_resource(MSPrediction4, '/MSPrediction4')
 
 if __name__ == '__main__':
-    flask_app.run(debug=True , host='0.0.0.0')
+    app.run(debug=True , host='0.0.0.0')
